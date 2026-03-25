b164fefa12a096097671861a831505be4e8e8331
Rainysun9025 <1528931062@qq.com>
2026-03-25 20:25:08 +0800

    p3817

--- 洛谷算法/算法1-5贪心/P3817 小A的糖果.cpp
+++ 洛谷算法/算法1-5贪心/P3817 小A的糖果.cpp
@@ -6,31 +6,9 @@ int main()
     cin>>n>>x;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
-    int sum=0;
-    for(int i=0;i<n;i++)
+    long long sum=0;
+    for(int i=1;i<n;i++)
     {
-        if(i==0)
-        {
-            if(arr[0]+arr[1]<=x) continue;
-            else if(arr[0]+arr[1]>x)
-            {
-                int dis=(arr[0]+arr[1])-x;
-                if(dis<=arr[1])
-                {
-                    sum+=dis;
-                    arr[1]-=dis;
-                }
-                else if(dis>arr[1])
-                {
-                    sum+=arr[1];
-                    dis-=arr[1];
-                    arr[1]=0;
-                    sum+=dis;
-                    arr[0]-=dis;
-                }
-            }
-            continue;
-        }
         if(arr[i]+arr[i-1]>x)
         {
             int dis=(arr[i]+arr[i-1])-x;
