db89b9bba60a570a6dbb04b9165948c5ea1069b0
Rainysun9025 <1528931062@qq.com>
2026-04-01 12:26:55 +0800

    .

--- ACM选修课/【算法2-1】前缀和、差分与离散化/P4552 [Poetize6] IncDec Sequence.cpp
+++ ACM选修课/【算法2-1】前缀和、差分与离散化/P4552 [Poetize6] IncDec Sequence.cpp
@@ -4,7 +4,21 @@ int main()
 {
     int n;
     cin>>n;
-    vector <int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-    
+    vector <int> arr(n+1);
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    vector <int> dif(n+1);
+    dif[1]=arr[1];
+    for(int i=2;i<=n;i++) dif[i]=arr[i]-arr[i-1];
+    long long up=0,low=0;
+    for(int i=2;i<=n;i++)
+    {
+        if(dif[i]<0) low-=dif[i];
+        else if(dif[i]>0) up+=dif[i];
+    }
+    // 最少步骤为 min(low,up)+|low-up|
+    //即max(low,up);
+    cout<<max(low,up)<<endl;
+    cout<<abs(low-up)+1<<endl;
+    return 0;
+
 }
