7f7bd218806964e5ef824dc253588db0a042152e
Rainysun9025 <1528931062@qq.com>
2026-05-27 19:01:38 +0800

    .

--- 文件的读写/test.cpp
+++ 文件的读写/test.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-void test01()
-{
-    ofstream ofs;
-    ofs.open("test.txt",ios::out);
-    ofs<<"你好"<<endl;
-}
+
 int main()
 {
-    test01();
+    ofstream ofs;
+    ofs.open("test.txt", ios::out);
+    if (!ofs) {   // 检查是否成功打开
+        cerr << "文件打开失败！" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i <= 1000; i++) {
+        ofs << "你好" << endl;
+    }
+
+    ofs.close();  // 显式关闭（也可以依赖析构函数自动关闭）
     return 0;
 }
